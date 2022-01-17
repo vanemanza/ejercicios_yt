57c8ec36b49e9dac7c501d8a76025e476cbc7ca7
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-01-17 00:15:15 -0300

    practicas python

---
+++ vane/leetcode.py
@@ -0,0 +1,14 @@
+"""
+Determine si un tablero de Sudoku de 9 x 9 es válido. 
+Solo las celdas llenas deben validarse de acuerdo con las siguientes reglas:
+
+:) Chequear que el tablero introducido sea un tablero 9x9 
+:) Cada fila debe contener los dígitos 1-9 sin repetición.
+:) Cada columna de'be contener los dígitos 1-9 sin repetición.
+:) Cada uno de los nueve subcuadros de 3 x 3 de la cuadrícula debe contener 
+los dígitos 1-9 sin repetición. 
+
+#modularizar -> módulos 
+"""
+
+'me duermo :('
